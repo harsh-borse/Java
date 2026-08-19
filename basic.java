bb47b82fab6e6acdeaa7e316a8c8773d22a95df5
harsh-borse <harshborse202372@gmail.com>
2026-08-19 17:07:45 +0530

    Assignment 1

--- basic.java
+++ basic.java
@@ -1,6 +1,19 @@
+import java.util.*;
 public class basic{
     public static void main(String[] args)
     {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.println("Enter the first number:" );
+        int a = sc.nextInt();
+        System.out.println("Enter the second number:" );
+        int b = sc.nextInt();
+
+        int sum = a + b;
+        System.out.println("The sum of two numbers is: " + sum);
+        
         System.out.println("Hello World");
         System.out.print("hii ");
     }
