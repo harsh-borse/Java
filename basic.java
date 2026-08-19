1ce8e5a0bb54f8c93025d43d7aa09642beefbf4e
harsh-borse <harshborse202372@gmail.com>
2026-08-19 17:46:04 +0530

    Assignment1

--- basic.java
+++ basic.java
@@ -6,15 +6,45 @@ public class basic{
         Scanner sc = new Scanner(System.in);
 
         
-        System.out.println("Enter the first number:" );
+        System.out.print("Enter the first number:" );
         int a = sc.nextInt();
-        System.out.println("Enter the second number:" );
+        System.out.print("Enter the second number:" );
         int b = sc.nextInt();
 
-        int sum = a + b;
-        System.out.println("The sum of two numbers is: " + sum);
+        System.out.println("Choose an Operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("5. Modulus");
         
-        System.out.println("Hello World");
-        System.out.print("hii ");
+        int ch = sc.nextInt();
+
+        switch(ch) {
+            case 1:
+                System.out.println("The Sum of two numbers is: " + (a + b));
+                break;
+            case 2:
+                System.out.println("The Subtraction of two numbers is: " + (a - b));
+                break;
+            case 3:
+                System.out.println("The Multiplication of two numbers is: " + (a * b));
+                break;
+            case 4:
+                if(b != 0) 
+                    System.out.println("The Division of two numbers is: " + (a / b));
+                else 
+                    System.out.println("Error! Division by zero is not allowed.");
+                break;
+            case 5:
+                if(b !=0)
+                    System.out.println("The Modulus of two numbers is: " + (a % b));
+                else 
+                    System.out.println("Error! Modulus by zero is not allowed.");
+                break;
+            default:
+                System.out.println("Invalid choice!");
+        }
+
     }
 }
